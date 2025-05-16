279703d1420d5497ea633ed123e20620e956b4d0
Isacee <alfonsofavio28@gmail.com>
2025-05-16 21:01:16 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -102,18 +102,3 @@ function collectStar(player, star) {
         });
     }
 }
-
-
-
-
-// {
-//     this.add.image(400, 300, 'sky');
-
-//     platforms = this.physics.add.staticGroup();
-
-//     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-//     platforms.create(600, 400, 'ground');
-//     platforms.create(50, 250, 'ground');
-//     platforms.create(750, 220, 'ground');
-// }
